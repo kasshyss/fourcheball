68f97fcfd383430682865658580febd7743f0022
Meriadoc <pierre@bourgain.net>
2018-01-25 11:21:13 +0100

    add FB feature

--- appli/fourcheball.py
+++ appli/fourcheball.py
@@ -10,6 +10,9 @@ from googleapiclient import discovery
 from pprint import pprint
 import httplib2
 
+from fbchat import Client
+from fbchat.models import *
+
 # init
 web_site = conf.get_conf('app.conf')['site']
 target = conf.get_conf('app.conf')['target']
@@ -67,10 +70,14 @@ body = {
 service.spreadsheets().values().update(spreadsheetId = spreadsheet_target, range=spreadsheet_range, valueInputOption = spreadsheet_value_input_option,body = body).execute()
 # get score
 result = service.spreadsheets().values().get(spreadsheetId = spreadsheet_target, range=spreadsheet_result).execute()
-fourcheball_result = [u'Fourcheballer | Point | Rang']
+fourcheball_result = 'Fourcheballer   Point   Rang\n'
 for item in  result['values']:
-    fourcheball_result.append(conf.add_space(item[0], 13) + ' | ' + conf.add_space(item[1], 5) + ' | ' + conf.add_space(item[2], 4))
-    
+    fourcheball_result = fourcheball_result + conf.add_space(item[0], 13) + '   ' + conf.add_space(item[1], 5) + '   ' + conf.add_space(item[2], 4) + '\n'
+
 print fourcheball_result
 
+fb_client = Client(conf.get_conf('app.conf')['fb_login'], conf.get_conf('app.conf')['fb_pass'])
+#fb_client.send(Message(text = 'Salut les nazes, c\'est l\'heure du classement'), thread_id=int(conf.get_conf('app.conf')['fb_message_id']), thread_type=ThreadType.GROUP)
+#fb_client.send(Message(text = fourcheball_result), thread_id=int(conf.get_conf('app.conf')['fb_message_id']), thread_type=ThreadType.GROUP)
+fb_client.logout()
 
