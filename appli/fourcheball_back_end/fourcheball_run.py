3fe2d98d9c222cbe0988c8597568ececa0cd9129
meriadoc <pierre@bourain.org>
2018-08-23 16:24:36 +0200

    add fb feature

--- appli/fourcheball_back_end/fourcheball_run.py
+++ appli/fourcheball_back_end/fourcheball_run.py
@@ -2,23 +2,25 @@
 #-*- coding: utf-8 -*-
 
 from flask import Flask
+from flask import request
 import bs4
 import json
 import requests
 import m_conf as conf
 import logging
+from fbchat import Client
+from fbchat.models import *
 
 app = Flask(__name__)
 logging.basicConfig()
 logger = logging.getLogger('fourcheball')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+app_conf = conf.get_conf('back_end.conf')
 logger.info('Init OK')
 
-# Init
-app_conf = conf.get_conf('back_end.conf')
 @app.route('/')
 def index():
-    return 'Index available services :\n    - /fourcheball_end_point/serv_L1/ranking_L1'
+    return 'Index available services :\n    - /fourcheball_end_point/serv_L1/ranking_L1\n       -/fourcheball_end_point/serv_fb/get_last_messages'
 
 # this url return L1 current ranking
 @app.route('/fourcheball_end_point/serv_L1/ranking_L1')
@@ -43,6 +45,34 @@ def ranking_l1():
     logger.debug('return dico')
     return json.dumps(l1_result, ensure_ascii=False)
 
+# return last 3 messages from a a fb conversation
+@app.route('/fourcheball_end_point/serv_fb/get_last_messages')
+def fb_messages():
+    logger.info('Get last fb messages : init')
+    fb_thread = int(app_conf['fb_thread'])
+    fb_client = Client(app_conf['fb_login'],app_conf['fb_password'])
+    logger.info('Get last fb messages : grab last messages')
+    last_messages = fb_client.fetchThreadMessages(thread_id = fb_thread, limit = 3)
+    msg = {} 
+    msg[0] = last_messages[2].text
+    msg[1] = last_messages[1].text
+    msg[2] = last_messages[0].text
+    logger.info('Get last fb messages : return result')
+    return json.dumps(msg, ensure_ascii=False)
+
+# send message to fb thread
+@app.route('/fourcheball_end_point/serv_fb/send_msg', )
+def send_fb_msg():
+    logger.info('Send new message to fb')
+    msg = request.args.get('msg')
+    logger.info('Message : {}'.format(msg))
+    fb_thread = int(app_conf['fb_thread'])
+    fb_client = Client(app_conf['fb_login'],app_conf['fb_password'])
+    fb_client.send(Message(text = msg), thread_id = fb_thread, thread_type = ThreadType.GROUP)
+    logger.info('Message posted')
+    return fb_messages()
+
+
 @app.route('/fourcheball_end_point/serv_L1/ranking_player')
-def ranking_player():
+def get_ranking_player():
     print 'TBD PLAYER RANKING'
