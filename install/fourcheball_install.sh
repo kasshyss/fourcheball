2e71adbe13b95856eaba0180b7dcabb3a816a13d
meriadoc <pierre@bourain.org>
2018-08-23 16:37:11 +0200

    remove unused features in install sh

--- install/fourcheball_install.sh
+++ install/fourcheball_install.sh
@@ -6,13 +6,3 @@ sudo pip install Flask
 sudo pip install requests
 sudo pip install fbchat
 sudo pip install --upgrade google-api-python-client oauth2client
-sudo apt-get install wget
-wget --quiet -0 https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install postgresql
-sudo apt-get install postgresql-client
-sudo adduser meriadoc
-sudo -u postgres createuser meriadoc
-sudo -u postgres createdb -O meriadoc fourcheball
-
